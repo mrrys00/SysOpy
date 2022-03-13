a3d5800bac6c7a94dfb89ee430d8fefa8a93b00d
Szymon Ryś <rysszymon00@gmail.com>
2022-03-13 19:41:20 +0100

    add temp changes

---
+++ cw01/zad01/counter.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#ifndef counter
+#define counter
+
+	extern char*** create_table(int size);
+	extern void count_all(FILE *fin1, FILE *fin2, FILE *fout);
+	extern int create_block(FILE *fp, char ***main, int id);
+	extern int block_size(char ***main, int id);
+//	extern void remove_line(char ***main, int id, int line);
+	extern void remove_block(char ***main, int id);
+//	extern void print(char ***main, int size);
+	extern void remove_all(char ***main, int size);
+
+#endif
