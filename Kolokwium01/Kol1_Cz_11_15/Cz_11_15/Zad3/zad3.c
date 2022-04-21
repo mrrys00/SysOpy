cf9db31b99868ca815cceaee7dbb1708acb1a4d0
Szymon Ryś <rysszymon00@gmail.com>
2022-04-21 11:42:05 +0200

    zad3

--- Kolokwium01/Kol1_Cz_11_15/Cz_11_15/Zad3/zad3.c
+++ Kolokwium01/Kol1_Cz_11_15/Cz_11_15/Zad3/zad3.c
@@ -1,121 +1,116 @@
 
 
 /* pipe_ls_wc.c
-   Program wykonuje instukcje potoku "ls | wc -l" 
+   Program wykonuje instukcje potoku "ls | wc -l"
    Uzupelnij wlasciwe instrukcje w miejscach *****
 */
 #include <sys/wait.h>
-#include <sys/types.h> 
-#include <stdio.h>      
-#include <stdlib.h>     
-#include <unistd.h>     
-#include <errno.h>      
-#include <string.h>     
- 
-int
-main(int argc, char *argv[])
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
 {
-    int pfd[2];                                     
+    int pfd[2];
 
-    if (pipe(pfd) == -1)                           
-        {
-         fprintf(stderr, "pipe");
-         exit(-1);
-        }
-    switch (fork()) {
-    case -1:
-         fprintf(stderr, "fork");
-         exit(-1);
-       
-    case 0:             
-        if (close(pfd[0]) == -1)                   
-            {
-             fprintf(stderr, "close 1");
-             exit(-1);
-           }
-        
-
-        if (pfd[1] != STDOUT_FILENO) {              
-            if (dup2(pfd[1], STDOUT_FILENO) == -1)
-                {
-                  fprintf(stderr, "dup2 1");
-                  exit(-1);
-                }
- 
-            if (/****** zamknij wlasciwy deskryptor */)
-                {
-                  fprintf(stderr, "close 2");
-                   exit(-1);
-                }
-
-
-
-        }
-
-        /***** Uruchom proces ls *****/      
-        
+    if (pipe(pfd) == -1)
+    {
+        fprintf(stderr, "pipe");
         exit(-1);
-              
-    default:           
+    }
+    switch (fork())
+    {
+    case -1:
+        fprintf(stderr, "fork");
+        exit(-1);
+
+    case 0:
+        if (close(pfd[0]) == -1)
+        {
+            fprintf(stderr, "close 1");
+            exit(-1);
+        }
+
+        if (pfd[1] != STDOUT_FILENO)
+        {
+            if (dup2(pfd[1], STDOUT_FILENO) == -1)
+            {
+                fprintf(stderr, "dup2 1");
+                exit(-1);
+            }
+
+            if (close(pfd[1]) == -1) /****** zamknij wlasciwy deskryptor */
+            {
+                fprintf(stderr, "close 2");
+                exit(-1);
+            }
+        }
+
+        /***** Uruchom proces ls *****/
+        execl("/bin/bash", "/bin/bash", "-c", "ls", NULL);
+
+        exit(-1);
+
+    default:
         break;
     }
 
-    switch (fork()) {
+    switch (fork())
+    {
     case -1:
-           fprintf(stderr, "fork");
-           exit(-1);
-    case 0:            
-        if (close(pfd[1]) == -1)                   
-            {         
+        fprintf(stderr, "fork");
+        exit(-1);
+    case 0:
+        if (close(pfd[1]) == -1)
+        {
             fprintf(stderr, "close 3");
             exit(-1);
-           }
+        }
 
-     
-      /***** uzyj dup2 z wlasciwymi argumentami *****/     
+        /***** uzyj dup2 z wlasciwymi argumentami *****/
+        dup2(pfd[0], STDIN_FILENO);
 
-       if (close(pfd[0]) == -1)
-                {
-                  fprintf(stderr, "close 4");
-                  exit(-1);
-                }
-
+        if (close(pfd[0]) == -1)
+        {
+            fprintf(stderr, "close 4");
+            exit(-1);
         }
 
         /***** uruchom proces wc -l *****/
+        execl("/bin/bash", "/bin/bash", "-c", "wc -l", NULL);
 
         exit(-1);
 
-    default: 
+    default:
         break;
     }
 
-
-
     if (close(pfd[0]) == -1)
-        {
-                  fprintf(stderr, "close 5");
-                   exit(-1);
-        }
-
+    {
+        fprintf(stderr, "close 5");
+        exit(-1);
+    }
 
     if (close(pfd[1]) == -1)
-        {
-                  fprintf(stderr, "close 6");
-                   exit(-1);
-        }
- 
-   if (wait(NULL) == -1)
-        {
-            fprintf(stderr, "wait 1");
-            exit(-1);
-        }
+    {
+        fprintf(stderr, "close 6");
+        exit(-1);
+    }
 
     if (wait(NULL) == -1)
-        {
-            fprintf(stderr, "wait 2");
-            exit(-1);
-        }
+    {
+        fprintf(stderr, "wait 1");
+        exit(-1);
+    }
+
+    if (wait(NULL) == -1)
+    {
+        fprintf(stderr, "wait 2");
+        exit(-1);
+    }
 
     exit(EXIT_SUCCESS);
 }
