75f98aac23fc9b9ab8c7a8458d75f6a3b5182c2a
Szymon Ryś <rysszymon00@gmail.com>
2022-05-22 18:48:16 +0200

    add finished lab08

--- cw08/zad1.c
+++ cw08/zad1.c
@@ -1,22 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <signal.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
 #include <sys/time.h>
-#include <sys/wait.h>
-#include <sys/file.h>
-#include <sys/msg.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/sem.h>
-#include <sys/mman.h>
-#include <limits.h>
-#include <time.h>
-#include <errno.h>
-#include <semaphore.h>
 #include <pthread.h>
 
 typedef struct
