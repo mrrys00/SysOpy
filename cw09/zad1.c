b25ea47ed08fad2c89b4b6e24a7dd8befac445d9
Szymon Ryś <rysszymon00@gmail.com>
2022-05-29 22:10:34 +0200

    temp working version

--- cw09/zad1.c
+++ cw09/zad1.c
@@ -6,31 +6,30 @@
 #include <sys/time.h>
 #include <pthread.h>
 
+int wait_cnt = 0, in_office_raindeers = 0, waiting_elfs_pids[3], actors_num = 20;
+pthread_t threads[20]; // mikołaj + 9 reniferów + 10 elfów
 
-pthread_t threads[20];  // mikołaj + 9 reniferów + 10 elfów
-int waitnum = 0, returned = 0, waiting[3];
-
-pthread_mutex_t wait_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t ret_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t quit_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t santa_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t waiting_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t return_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t exit_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t santa_claus_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+// pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
-pthread_cond_t santa_cond = PTHREAD_COND_INITIALIZER;
-pthread_cond_t solved = PTHREAD_COND_INITIALIZER;
-pthread_cond_t gifts = PTHREAD_COND_INITIALIZER;
-pthread_cond_t quit = PTHREAD_COND_INITIALIZER;
+pthread_cond_t santa_claus_cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t elf_problem_solved = PTHREAD_COND_INITIALIZER;
+pthread_cond_t give_gifts = PTHREAD_COND_INITIALIZER;
+pthread_cond_t exitt = PTHREAD_COND_INITIALIZER;
 
 void safe_exit(int signo)
 {
-    // "You can't have data of your own passed to the signal handler as parameters. Instead you'll have to store your parameters in global variables." ~ https://stackoverflow.com/questions/6970224/providing-passing-argument-to-signal-handler 
+    // "You can't have data of your own passed to the signal handler as parameters. Instead you'll have to store your parameters in global variables." ~ https://stackoverflow.com/questions/6970224/providing-passing-argument-to-signal-handler
     if (signo == SIGINT)
     {
-        for (int i = 0; i < 20; i++)
+        for (int i = 0; i < actors_num; i++)
             pthread_cancel(threads[i]);
-        pthread_cond_broadcast(&quit);
+        pthread_cond_broadcast(&exitt);
     }
     return;
 }
@@ -42,37 +41,42 @@ void sweet_dreams(double mini, double maxi)
     return;
 }
 
-void *santa_thread(void *varg)
+void *santa_claus_thread(void *varg)
 {
     while (1)
     {
-        pthread_mutex_lock(&santa_mutex);
-        pthread_cond_wait(&santa_cond, &santa_mutex);
-        pthread_mutex_unlock(&santa_mutex);
+        // schemat z UPEL:
+        // 1. blokujemy dostęp do zasobów współdzielonych
+        pthread_mutex_lock(&santa_claus_mutex);
+        // 2. operujemy na współdzielonym obiekcie
+        pthread_cond_wait(&santa_claus_cond, &santa_claus_mutex);
+        // 3. odbkolowujemy
+        pthread_mutex_unlock(&santa_claus_mutex);
 
-        pthread_mutex_lock(&ret_mutex);
-        if (returned == 9)
+        pthread_mutex_lock(&return_mutex);
+        if (in_office_raindeers == 9)
         {
             printf("Mikołaj: budzę się\n");
             printf("Mikołaj: dostarczam zabawki\n");
+            // Dostarczają zabawki grzecznym dzieciom
             sweet_dreams(2.0, 4.0);
-            returned = 0;
+            in_office_raindeers = 0;
             printf("Mikołaj: zasypiam\n");
         }
-        pthread_mutex_unlock(&ret_mutex);
-        pthread_cond_broadcast(&gifts);
+        pthread_mutex_unlock(&return_mutex);
+        pthread_cond_broadcast(&give_gifts);
 
-        pthread_mutex_lock(&wait_mutex);
-        if (waitnum == 3)
+        pthread_mutex_lock(&waiting_mutex);
+        if (wait_cnt == 3)
         {
             printf("Mikołaj: budzę się\n");
-            printf("Mikołaj: rozwiązuje problemy elfów %d %d %d ID\n", waiting[0], waiting[1], waiting[2]);
+            printf("Mikołaj: rozwiązuje problemy elfów %d %d %d ID\n", waiting_elfs_pids[0], waiting_elfs_pids[1], waiting_elfs_pids[2]);
             sweet_dreams(1.0, 2.0);
-            waitnum = 0;
+            wait_cnt = 0;
             printf("Mikołaj: zasypiam\n");
         }
-        pthread_mutex_unlock(&wait_mutex);
-        pthread_cond_broadcast(&solved);
+        pthread_mutex_unlock(&waiting_mutex);
+        pthread_cond_broadcast(&elf_problem_solved);
     }
 }
 
@@ -81,27 +85,33 @@ void *elf_thread(void *varg)
     int id = *((int *)varg);
     while (1)
     {
+        // Pracują przez losowy okres czasu (2-5s).
         sweet_dreams(2.0, 5.0);
-        pthread_mutex_lock(&wait_mutex);
-        if (waitnum < 3)
+        // Chcą zgłosić problem
+        pthread_mutex_lock(&waiting_mutex);
+        if (wait_cnt < 3)
         {
             printf("Elf: czeka na powrót elfów, %d\n", id);
         }
-        while (waitnum >= 3)
+        // Mikołaj się z nimi spotyka.
+        while (wait_cnt >= 3)
         {
             printf("Elf: Mikołaj rozwiązuje problem, %d\n", id);
-            pthread_cond_wait(&solved, &wait_mutex);
+            pthread_cond_wait(&elf_problem_solved, &waiting_mutex);
         }
-        
-        waiting[waitnum++] = id;
-        printf("Elf: czeka %d elfów na Mikołaja, %d\n", waitnum, id);
-        if (waitnum == 3)
+
+        waiting_elfs_pids[wait_cnt++] = id;
+        printf("Elf: czeka %d elfów na Mikołaja, %d\n", wait_cnt, id);
+
+        // Jeśli jest trzecim elfem przed warsztatem to wybudza Mikołaja
+        if (wait_cnt == 3)
         {
             printf("Elf: wybudzam Mikołaja, %d\n", id);
-            pthread_cond_broadcast(&santa_cond);
+            pthread_cond_broadcast(&santa_claus_cond);
         }
-        pthread_cond_wait(&solved, &wait_mutex);
-        pthread_mutex_unlock(&wait_mutex);
+        pthread_cond_wait(&elf_problem_solved, &waiting_mutex);
+        // Wraca do pracy
+        pthread_mutex_unlock(&waiting_mutex);
     }
 }
 
@@ -110,17 +120,22 @@ void *raindeer_thread(void *varg)
     int id = *((int *)varg);
     while (1)
     {
+        // Są na wakacjach w ciepłych krajach losowy okres czasu (5-10s)
         sweet_dreams(5.0, 10.0);
-        pthread_mutex_lock(&ret_mutex);
-        returned++;
-        printf("Renifer: czeka %d reniferów na Mikołaja, %d\n", id, returned);
-        if (waitnum == 9)
+        // Wracaja na biegun północny
+        pthread_mutex_lock(&return_mutex);
+        in_office_raindeers++;
+        printf("Renifer: czeka %d reniferów na Mikołaja, %d\n", id, in_office_raindeers);
+        if (wait_cnt == 9)
         {
             printf("Renifer: wybudzam Mikołaja, %d\n", id);
-            pthread_cond_broadcast(&santa_cond);
+            // Dostarczają zabawki grzecznym dzieciom
+            pthread_cond_broadcast(&santa_claus_cond);
+            in_office_raindeers = 0;
         }
-        pthread_cond_wait(&gifts, &ret_mutex);
-        pthread_mutex_unlock(&ret_mutex);
+        pthread_cond_wait(&give_gifts, &return_mutex);
+        // Lecą na wakacje
+        pthread_mutex_unlock(&return_mutex);
     }
 }
 
@@ -128,23 +143,24 @@ int main()
 {
     srand(time(NULL));
     signal(SIGINT, safe_exit);
-    pthread_t *santa = threads, *elf = &threads[1], *raindeer = &threads[11];
+    // poniżej wskazujemy na konkretne elementy w tablicy 
+    pthread_t *santa_claus_ptr = threads, *elf_ptr = &threads[10], *raindeer_ptr = &threads[1];
     int elfs_num = 10, raindeer_num = 9;
     int IDs[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    //stworzenie mikołaja
-    pthread_create(santa, NULL, santa_thread, NULL);
+    // stworzenie mikołaja
+    pthread_create(santa_claus_ptr, NULL, santa_claus_thread, NULL);
 
     // tworzenie elfów
     for (int i = 0; i < elfs_num; i++)
-        pthread_create(&elf[i], NULL, elf_thread, (void *)&IDs[i]);
+        pthread_create(&elf_ptr[i], NULL, elf_thread, (void *)&IDs[i]);
 
     // tworzenie reniferów
     for (int i = 0; i < raindeer_num; i++)
-        pthread_create(&raindeer[i], NULL, raindeer_thread, (void *)&IDs[i]);
+        pthread_create(&raindeer_ptr[i], NULL, raindeer_thread, (void *)&IDs[i]);
 
     // mutex wyjścia z programu
-    pthread_cond_wait(&quit, &quit_mutex);
+    pthread_cond_wait(&exitt, &exit_mutex);
 
     exit(EXIT_SUCCESS);
 }
