6084f6db1f166999a37adcbbc24cfbbaf4e3e83f
szymon.rys <szymon.rys@awesome-ind.com>
2022-05-30 12:26:21 +0200

    add finished lab09

--- cw09/zad1.c
+++ cw09/zad1.c
@@ -6,16 +6,13 @@
 #include <sys/time.h>
 #include <pthread.h>
 
-int wait_cnt = 0, in_office_raindeers = 0, waiting_elfs_pids[3], actors_num = 20;
+int waiting_elfs_cnt = 0, in_office_raindeers = 0, waiting_elfs_pids[3], actors_num = 20;
 pthread_t threads[20]; // mikołaj + 9 reniferów + 10 elfów
 
 pthread_mutex_t waiting_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t return_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t exit_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t santa_claus_mutex = PTHREAD_MUTEX_INITIALIZER;
-
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-// pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 pthread_cond_t santa_claus_cond = PTHREAD_COND_INITIALIZER;
 pthread_cond_t elf_problem_solved = PTHREAD_COND_INITIALIZER;
@@ -27,6 +24,63 @@ void sweet_dreams(double mini, double maxi)
     // randomized sleep time in µseconds
     usleep(((rand() % 1000) * 1000) * (maxi - mini) + mini * 1000000);
     return;
+}
+
+void *elf_thread(void *varg)
+{
+    int id = *((int *)varg);
+    while (1)
+    {
+        // Pracują przez losowy okres czasu (2-5s).
+        sweet_dreams(2.0, 5.0);
+        // Chcą zgłosić problem
+        pthread_mutex_lock(&waiting_mutex);
+        if (waiting_elfs_cnt < 3)
+            printf("Elf: czeka na powrót elfów, %d\n", id);
+        // Mikołaj się z nimi spotyka.
+        while (waiting_elfs_cnt >= 3)
+        {
+            printf("Elf: Mikołaj rozwiązuje problem, %d\n", id);
+            pthread_cond_wait(&elf_problem_solved, &waiting_mutex);
+        }
+
+        waiting_elfs_pids[waiting_elfs_cnt++] = id;
+        printf("Elf: czeka %d elfów na Mikołaja, %d\n", waiting_elfs_cnt, id);
+
+        // Jeśli jest trzecim elfem przed warsztatem to wybudza Mikołaja
+        if (waiting_elfs_cnt == 3)
+        {
+            printf("Elf: wybudzam Mikołaja, %d\n", id);
+            pthread_cond_broadcast(&santa_claus_cond);
+        }
+        pthread_cond_wait(&elf_problem_solved, &waiting_mutex);
+        // Wraca do pracy
+        pthread_mutex_unlock(&waiting_mutex);
+    }
+}
+
+void *raindeer_thread(void *varg)
+{
+    int id = *((int *)varg);
+    while (1)
+    {
+        // Są na wakacjach w ciepłych krajach losowy okres czasu (5-10s)
+        sweet_dreams(5.0, 10.0);
+        // Wracaja na biegun północny
+        pthread_mutex_lock(&return_mutex);
+        in_office_raindeers++;
+        printf("Renifer: czeka %d reniferów na Mikołaja, %d\n", id, in_office_raindeers);
+        if (in_office_raindeers == 9)
+        {
+            printf("Renifer: wybudzam Mikołaja, %d\n", id);
+            // Dostarczają zabawki grzecznym dzieciom
+            pthread_cond_broadcast(&santa_claus_cond);
+            in_office_raindeers = 0;
+        }
+        pthread_cond_wait(&give_gifts, &return_mutex);
+        // Lecą na wakacje
+        pthread_mutex_unlock(&return_mutex);
+    }
 }
 
 void *santa_claus_thread(void *varg)
@@ -55,75 +109,16 @@ void *santa_claus_thread(void *varg)
         pthread_cond_broadcast(&give_gifts);
 
         pthread_mutex_lock(&waiting_mutex);
-        if (wait_cnt == 3)
+        if (waiting_elfs_cnt == 3)
         {
             printf("Mikołaj: budzę się\n");
             printf("Mikołaj: rozwiązuje problemy elfów %d %d %d ID\n", waiting_elfs_pids[0], waiting_elfs_pids[1], waiting_elfs_pids[2]);
             sweet_dreams(1.0, 2.0);
-            wait_cnt = 0;
+            waiting_elfs_cnt = 0;
             printf("Mikołaj: zasypiam\n");
         }
         pthread_mutex_unlock(&waiting_mutex);
         pthread_cond_broadcast(&elf_problem_solved);
-    }
-}
-
-void *elf_thread(void *varg)
-{
-    int id = *((int *)varg);
-    while (1)
-    {
-        // Pracują przez losowy okres czasu (2-5s).
-        sweet_dreams(2.0, 5.0);
-        // Chcą zgłosić problem
-        pthread_mutex_lock(&waiting_mutex);
-        if (wait_cnt < 3)
-        {
-            printf("Elf: czeka na powrót elfów, %d\n", id);
-        }
-        // Mikołaj się z nimi spotyka.
-        while (wait_cnt >= 3)
-        {
-            printf("Elf: Mikołaj rozwiązuje problem, %d\n", id);
-            pthread_cond_wait(&elf_problem_solved, &waiting_mutex);
-        }
-
-        waiting_elfs_pids[wait_cnt++] = id;
-        printf("Elf: czeka %d elfów na Mikołaja, %d\n", wait_cnt, id);
-
-        // Jeśli jest trzecim elfem przed warsztatem to wybudza Mikołaja
-        if (wait_cnt == 3)
-        {
-            printf("Elf: wybudzam Mikołaja, %d\n", id);
-            pthread_cond_broadcast(&santa_claus_cond);
-        }
-        pthread_cond_wait(&elf_problem_solved, &waiting_mutex);
-        // Wraca do pracy
-        pthread_mutex_unlock(&waiting_mutex);
-    }
-}
-
-void *raindeer_thread(void *varg)
-{
-    int id = *((int *)varg);
-    while (1)
-    {
-        // Są na wakacjach w ciepłych krajach losowy okres czasu (5-10s)
-        sweet_dreams(5.0, 10.0);
-        // Wracaja na biegun północny
-        pthread_mutex_lock(&return_mutex);
-        in_office_raindeers++;
-        printf("Renifer: czeka %d reniferów na Mikołaja, %d\n", id, in_office_raindeers);
-        if (wait_cnt == 9)
-        {
-            printf("Renifer: wybudzam Mikołaja, %d\n", id);
-            // Dostarczają zabawki grzecznym dzieciom
-            pthread_cond_broadcast(&santa_claus_cond);
-            in_office_raindeers = 0;
-        }
-        pthread_cond_wait(&give_gifts, &return_mutex);
-        // Lecą na wakacje
-        pthread_mutex_unlock(&return_mutex);
     }
 }
 
@@ -147,18 +142,18 @@ int main()
     // poniżej wskazujemy na konkretne elementy w tablicy 
     pthread_t *santa_claus_ptr = threads, *elf_ptr = &threads[10], *raindeer_ptr = &threads[1];
     int elfs_num = 10, raindeer_num = 9;
-    int IDs[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int ids_creator[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     // stworzenie mikołaja
     pthread_create(santa_claus_ptr, NULL, santa_claus_thread, NULL);
 
     // tworzenie elfów
     for (int i = 0; i < elfs_num; i++)
-        pthread_create(&elf_ptr[i], NULL, elf_thread, (void *)&IDs[i]);
+        pthread_create(&elf_ptr[i], NULL, elf_thread, (void *)&ids_creator[i]);
 
     // tworzenie reniferów
     for (int i = 0; i < raindeer_num; i++)
-        pthread_create(&raindeer_ptr[i], NULL, raindeer_thread, (void *)&IDs[i]);
+        pthread_create(&raindeer_ptr[i], NULL, raindeer_thread, (void *)&ids_creator[i]);
 
     // mutex wyjścia z programu
     pthread_cond_wait(&exitt, &exit_mutex);
