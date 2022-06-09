e0212e5f1e11bfcb4dcf6d3b4d8fa29c339b94fd
piotrekvs <pvanselow.2000@poczta.fm>
2022-06-09 11:59:39 +0200

    zad1

--- Kolokwium2/Kol2_Cz_11_15/Zad1/main.c
+++ Kolokwium2/Kol2_Cz_11_15/Zad1/main.c
@@ -7,40 +7,48 @@
 
 #define THREADS_NR 9
 
-void *hello(void *arg) {
-//Napisz funkcję wypisującą komunikat typu Hello  wartość_arg TID: numer_TID wątku
-// i zwracającą status o wartości równej agrumentowi przesłanemu do funkcji
-
-
-
-
-
-
-
-
-//koniec
+void *hello(void *arg)
+{
+    // Napisz funkcję wypisującą komunikat typu Hello  wartość_arg TID: numer_TID wątku
+    //  i zwracającą status o wartości równej agrumentowi przesłanemu do funkcji
+    int arg_val = *(int *)arg;
+    printf("Hello %d TID: %ld\n", arg_val, pthread_self());
+    return arg;
+    // koniec
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     pthread_t hello_threads[THREADS_NR];
     int *count = malloc(sizeof(int));
     int i;
 
-    for (i=0; i<THREADS_NR;i++) {
-     *count=i+1;
-     int result = pthread_create(&hello_threads[i], NULL, hello, (void *) count);
-     if (result != 0) {
-         perror("Could not create thread.");
-         }
-     usleep(1000);
-     }
+    for (i = 0; i < THREADS_NR; i++)
+    {
+        *count = i + 1;
+        int result = pthread_create(&hello_threads[i], NULL, hello, (void *)count);
+        if (result != 0)
+        {
+            perror("Could not create thread.");
+        }
+        usleep(1000);
+    }
     int *hello_results[THREADS_NR];
-// Czekaj na zakończenie THREADS_NR wątków, pobierz status zakończeńczenia, zapisz go w  hello_result
-// i wypisz na ekran: Thread (indeks wątku+1) TID: (nr_TID) returned value: (status zakonczenia watku)
-
-
-
-
-//koniec
+    // Czekaj na zakończenie THREADS_NR wątków, pobierz status zakończeńczenia, zapisz go w  hello_result
+    // i wypisz na ekran: Thread (indeks wątku+1) TID: (nr_TID) returned value: (status zakonczenia watku)
+    for (int i = 0; i < THREADS_NR; i++)
+    {
+        if (pthread_join(hello_threads[i], (void**) &hello_results[i]) != 0)
+        {
+            err(EXIT_FAILURE, "pthread_join");
+        }
+    }
+    printf("\n\n");
+    for (int i = 0; i < THREADS_NR; i++)
+    {
+        printf("Thread %d TID: %d returned value: %d\n", i+1, count[i], *(int *)hello_results[i]);
+    }
+    free(count);
+    // koniec
     return 0;
 }
