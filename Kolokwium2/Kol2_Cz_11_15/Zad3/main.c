9f9f4499040519b87c5a7d4c0bcdc9e1f4529b84
Szymon Ryś <rysszymon00@gmail.com>
2022-06-09 11:43:37 +0200

    add 3

--- Kolokwium2/Kol2_Cz_11_15/Zad3/main.c
+++ Kolokwium2/Kol2_Cz_11_15/Zad3/main.c
@@ -12,37 +12,45 @@ int count = 0;
  * przez mutex 'mutex' osiągnie wartość co najmniej 13000, a następnie zwiększyć ją o 13.
  */
 
-void* wait_for_counter(void* arg) {
+void *wait_for_counter(void *arg)
+{
 
-//...
+    while (count <= 13000)
+    {
+    }
+    pthread_mutex_lock(&mutex);
 
     printf("Changing value: %d\n", count);
     count += 13;
+    pthread_mutex_unlock(&mutex);
 
-//...
-
-    pthread_exit(NULL); 
+    pthread_exit(NULL);
 }
 
-int main(void) {
-    for (int k = 0; k < 100; ++ k) {
+int main(void)
+{
+    for (int k = 0; k < 100; ++k)
+    {
         count = 0;
 
         pthread_t tid;
         /* Stwórz nowy wątek wykonujący funkcje wait_for_counter.
-	*/
+         */
+        pthread_create(&tid, NULL, wait_for_counter, NULL);
 
         int reps = 100000;
-        for (int i = 0; i < reps; ++ i) {
+        for (int i = 0; i < reps; ++i)
+        {
             pthread_mutex_lock(&mutex);
-            ++ count;
+            ++count;
             pthread_cond_signal(&var);
             pthread_mutex_unlock(&mutex);
         }
         pthread_join(tid, NULL);
 
         pthread_mutex_lock(&mutex);
-        if (count != reps + 13) {
+        if (count != reps + 13)
+        {
             printf("Wrong value: %d\n", count);
             exit(-1);
         }
